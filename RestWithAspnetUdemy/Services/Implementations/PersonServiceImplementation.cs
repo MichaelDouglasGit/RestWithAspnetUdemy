0332f8b4e8b15548a07b7a3670f0e20399f14bcd
Michael Douglas <94067749+MichaelDouglasGit@users.noreply.github.com>
2021-12-22 16:15:31 -0300

    database persistence adjustment

--- RestWithAspnetUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithAspnetUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -16,7 +16,15 @@ namespace RestWithAspnetUdemy.Services.Implementations
         {
             _context = context;
         }
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
 
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
         public Person Create(Person person)
         {
             try
@@ -31,41 +39,44 @@ namespace RestWithAspnetUdemy.Services.Implementations
             }
             return person;
         }
-
-        public void Delete(long id)
-        {
-            
-        }
-
-        public List<Person> FindAll()
-        {
-            return _context.Persons.ToList();
-        }
-
-        public Person FindByID(long id)
-        {
-            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
-        }
-
         public Person Update(Person person)
         {
             if (!Exists(person.Id)) return new Person();
-            
-            var result = _context.Persons.Any(p => p.Id.Equals(person.Id));
 
-            try
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
             {
-                _context.Add(person);
-                _context.SaveChanges();
-            }
-            catch (Exception ex)
-            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
 
-                throw ex;
+                    throw ex;
+                }
             }
+
             return person;
         }
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
 
+                    throw ex;
+                }
+            }
+        }
         private bool Exists(long id)
         {
             return _context.Persons.Any(p => p.Id.Equals(id));
