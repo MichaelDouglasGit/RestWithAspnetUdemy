fe02a9728a69d9568ba9647fa4270350906b4f77
Michael Douglas <94067749+MichaelDouglasGit@users.noreply.github.com>
2021-12-27 22:19:44 -0300

    add Migration

--- RestWithAspnetUdemy/Startup.cs
+++ RestWithAspnetUdemy/Startup.cs
@@ -9,27 +9,40 @@ using RestWithAspnetUdemy.Business;
 using RestWithAspnetUdemy.Business.Implementations;
 using RestWithAspnetUdemy.Repository;
 using RestWithAspnetUdemy.Repository.Implementations;
+using Serilog;
+using System;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace RestWithAspnetUdemy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+                
         }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddControllers();
 
             var connection = Configuration["SqlServerConnection:SqlServerConnectionString"];
 
             services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
 
             //Versioning API
             services.AddApiVersioning();
@@ -57,6 +70,25 @@ namespace RestWithAspnetUdemy
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new SqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
         }
     }
 }
---
+++ RestWithAspnetUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO [DBO].[PERSON]
+			   ([ADDRESS]
+			   ,[FIRST_NAME]
+			   ,[LAST_NAME]
+			   ,[GENDER])
+VALUES
+('Maringá, PR - Brasil','Octavio','Marques','Male'),
+('Porbandar - India','Indira','Gandhi','Female')
